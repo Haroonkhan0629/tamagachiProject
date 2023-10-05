c74785c34bd29369b9ef553a7ac5468cae9c406f
Haroon Khan <hronkkhan6@gmail.com>
2023-10-05 10:34:39 -0700

    finishing touches

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let titleEl = document.getElementById("title")
 let petName = prompt("What is your pet's name")
 titleEl.innerHTML = `${petName}`
@@ -14,13 +13,23 @@ class Tamagotchi {
 
 }
 
+class Tamagotchi2 {
+    constructor() {
+        this.age = 1
+    }
+}
+
 const pet = new Tamagotchi()
+const baby = new Tamagotchi2()
 
 intervalTimer = setInterval(function () {
     document.getElementById("age").textContent = pet.age += 1
     document.getElementById("hunger").textContent = pet.hunger += 1
     document.getElementById("boredom").textContent = pet.boredom += 1
     document.getElementById("sleepiness").textContent = pet.sleepiness += 1
+    if (pet.age >= 10) {
+        document.getElementById("baby-tag").textContent = `Baby's Age: ${baby.age += 1}`
+    }
     if (pet.hunger >= 10 || pet.sleepiness >= 10 || pet.boredom >= 10) {
         $("body").fadeOut(3000, function () {
             $("body").css({
@@ -33,10 +42,11 @@ intervalTimer = setInterval(function () {
             $("h1").html("<h1>YOUR PET DIED</h1>").fadeIn(3000)
         })
         $("h3").fadeOut(3000, function () {
-            $("h3").html(" <a href=/game.html <h1> Play Again </h1> </a>").fadeIn(3000)
+            $("h3:eq(0)").html(" <a href=/game.html <h1> Play Again </h1> </a>").fadeIn(3000)
         })
         $("button").fadeOut(3000)
         $("#pet").fadeOut(3000)
+        $("#baby").fadeOut(3000)
     }
 }, 5000)
 
@@ -44,6 +54,12 @@ intervalTimer = setTimeout(function () {
     alert("Your pet has transformed!")
     document.getElementById("pet").src = "https://media.giphy.com/media/sPD8vBqQxd4s0/giphy.gif"
 }, 20000)
+
+intervalTimer = setTimeout(function () {
+    alert("Your pet had a baby!")
+    document.getElementById("baby").src = "https://media.giphy.com/media/CBvjuhJncUt6U/200.gif"
+    $("#baby-tag").html("<h3>Baby's Age: </h3>").fadeIn(1000)
+}, 45000)
 
 function decreaseHunger() {
     if (pet.hunger === 1) {
@@ -97,5 +113,17 @@ document.getElementById("pet").animate(
     {
         duration: 8000,
         iterations: Infinity,
+    }
+)
+
+document.getElementById("baby").animate(
+    [
+        { transform: "translateX(0px)" },
+        { transform: "translateX(-200px)" },
+        { transform: "translateX(50px)" }
+    ],
+    {
+        duration: 8000,
+        iterations: Infinity
     }
 )
